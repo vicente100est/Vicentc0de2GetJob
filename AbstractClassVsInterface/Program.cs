7f1e274f313cf0d777b306553cea417de6c1f607
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-12-26 13:40:44 -0600

    Initial commit Add abstract class vs interface

---
+++ AbstractClassVsInterface/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public abstract class Bird
+{
+    public int _wings;
+    public Bird(int wings) => _wings = wings;
+}
+
+public interface IFly
+{
+    void Fly();
+}
+
+public interface IRun
+{
+    int Legs { get; set; }
+    void Run();
+}
+
+public interface ISwim
+{
+    void Swim();
+}
+
+public class Ostrich : Bird, IRun, ISwim
+{
+    private int _legs;
+
+    public int Legs
+    {
+        get { return _legs; }
+        set { _legs = value; }
+    }
+
+    public Ostrich(int wing) : base (wing)
+    {
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Run my Friend");
+    }
+
+    public void Swim()
+    {
+        Console.WriteLine("Swim Pajaro");
+    }
+}
